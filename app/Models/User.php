83ff854aedac19f6f9352d6c4011357293010b86
Adad Oviedo <josueoviedo83@aragon.unam.mx>
2024-02-26 19:28:02 -0600

    App Inicial

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,4 +42,21 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function adminlte_image()
+    {
+        return 'https://picsum.photos/300/300';
+    }
+
+    public function adminlte_desc()
+    {
+        return 'That\'s a nice guy';
+    }
+
+    public function adminlte_profile_url()
+    {
+        return 'profile/username';
+    }
+
 }
+
